ba59f1f6cb7ae63d733395a0f096e8708057b8ce
Edwin Edwin <alisauskas.edvinas@gmail.com>
2018-09-11 05:29:32 +0000

    automatically backed up by learn

---
+++ window.js
@@ -0,0 +1,19 @@
+// declare the variable
+var word
+ 
+// assign a value to the variable
+word = 'bird'
+ 
+console.log(word) // 'bird'
+ 
+// assign another value to the variable
+word = 'dog'
+ 
+console.log(word) // 'dog'
+
+
+
+
+Function sayHello() {
+  console.log('Hello!')
+}
